d8e67ada43a04ac5afa40d8e6a02358290255252
Romain Blanco <romain.blanco@gmail.com>
2019-02-12 12:16:07 +0800

    chg

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_param
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
